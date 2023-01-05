3848a550b4bf26bfee6c7c150edf37099d0795f3
Антон Гаврюхин <ah21@mail.ru>
2023-01-05 12:17:04 +0300

    task four

---
+++ task.js
@@ -0,0 +1,42 @@
+function Student(name, gender, age) {
+
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+
+}
+
+Student.prototype.setSubject = function (subjectName) {
+
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+      this.marks = [...marks];
+    } else {
+      this.marks.push(...marks);
+    }
+  };
+};
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach((mark) => (sum += parseInt(mark)));
+  return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
+
+// ваш код для остальных методов
